b2cb88fb51d4d1b4742f07e2fb6aa8e240dda2cb
Zack Scholl <zack.scholl@gmail.com>
2020-12-05 10:37:59 +0700

    allow uploading/downloading to norns.online

--- cheat_codes_2.lua
+++ cheat_codes_2.lua
@@ -22,6 +22,7 @@ rnd = include 'lib/rnd_actions'
 del = include 'lib/delay'
 rytm = include 'lib/euclid'
 mc = include 'lib/midicheat'
+sharer = include 'lib/sharer'
 math.randomseed(os.time())
 variable_fade_time = 0.01
 
@@ -721,7 +722,7 @@ zilch_leds =
 }
 
 function init()
-
+  sharer.setup("cheat_codes_2")
   clock.run(check_page_for_k1)
 
   collection_loaded = false
